aad6ee4ba31ef6a7df5236e396a12d360d3be715
Matt Emery <matthew.emery44@gmail.com>
2019-05-05 16:19:50 -0700

    Added Poisson example

---
+++ poisson.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from statsmodels.genmod.families import Poisson
+from statsmodels.api import GLM
+import statsmodels.api as sm
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import make_pipeline
+from functools import partial
+from horsekickpy.horsekick import clean_horse_kicks
+import numpy as np
+
+df = pd.read_csv("data/VonBort.csv")
+
+# https://stackoverflow.com/a/48949667/2687504
+class SMWrapper(BaseEstimator, RegressorMixin):
+    """ A universal sklearn-style wrapper for statsmodels regressors """
+    def __init__(self, model_class, fit_intercept=True):
+        self.model_class = model_class
+        self.fit_intercept = fit_intercept
+
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        self.model_ = self.model_class(y, X)
+        self.results_ = self.model_.fit()
+
+    def predict(self, X):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        return self.results_.predict(X)
+
+
+PoissonRegressor = partial(GLM, family=Poisson())
+horse_kick_pipeline = make_pipeline(OneHotEncoder(sparse=False),
+                                    SMWrapper(PoissonRegressor, fit_intercept=True))
+
+clean_df = clean_horse_kicks(df)
+
+horse_kick_pipeline.fit(np.asarray(clean_df), np.asarray(df["deaths"]))
+
+print(horse_kick_pipeline.predict(clean_df))
+
+
